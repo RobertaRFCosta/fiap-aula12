4c493d169a79f50b06ee8fe02c96df2aca093563
Roberta <roberta.rfcosta@gmail.com>
2024-05-08 21:35:49 -0300

    class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,21 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'aula12';
+
+//   // desabilitado: boolean = true;
+
+//   imageUrl: String = "http://lorempixel.com.br/200/200";
+// }
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,9 +27,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'aula12';
+  title = 'introducao-angular';
 
-  // desabilitado: boolean = true;
-
-  imageUrl: String = "http://lorempixel.com.br/200/200";
+  urlImagem = 'http://lorempixel.com.br/400/200';
 }
